f8c5947b119f786d1aa2700bfaa09bd37fd23749
tarakkomera <tarakkomera9160@gmail.com>
2026-09-15 01:13:34 +0530

    feat: customize Telugu romantic confession for HimaVarshini

--- src/data/secretMessage.ts
+++ src/data/secretMessage.ts
@@ -9,7 +9,7 @@ export const PARTNER_PHONE_NUMBER = '918179170861'
 
 export const SECRET_CONFESSION_MESSAGE = `May be ee vishyam Neku Ela chepali ento ardham kavatledu but chepaka pothey inka enii days aina enii years ee vishyam Neku chepalenu emo ani bhayam vesthundhi andhuke chepali ani anukuntuna may be deni taruvatha nuv natho matladathavo ledo kuda naku telidhu but I just want convey this to you.
 
-From past few Years nunchi  nuv antey naku istam. Nenu ninnu chudaka mundhu nunche nuv antey istam. May be adhi tappu aye unda ochu oka ammai permission lekunda edhi antha cheyadam but naku telidhu edhi tappa ledha correct aa ani but nenu aitey ninnu ista paduthune una I just imagined my life with you. Nenu chala Sarlu anukuna like i not correct for you (i mean that by looks and I think You dis-likes me like that many questions had run inside me).But i don't know why everything wants to throws towards you. It's been 6 years neku chepakunda nalo nenu ista paduthu undhi inka chepakunda elane unta emo ani oka bhayam tho chepthuna.
+From past few Years nunchi nuv antey naku istam. Nenu ninnu chudaka mundhu nunche nuv antey istam. May be adhi tappu aye unda ochu oka ammai permission lekunda edhi antha cheyadam but naku telidhu edhi tappa ledha correct aa ani but nenu aitey ninnu ista paduthune una I just imagined my life with you. Nenu chala Sarlu anukuna like i not correct for you (i mean that by looks and I think You dis-likes me like that many questions had ran inside me).But i don't know why everything wants to throws towards you. It's been 6 years neku chepakunda nalo nenu ista paduthu undhi inka chepakunda elane unta emo ani oka bhayam tho chepthuna.
 
 Nee medha yentha istam undhii ani nenu express cheyaledhu.  
 
